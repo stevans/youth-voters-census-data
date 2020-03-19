5185b08e20ab31e144127327a3843152f03c2802
Matt Stevans <stevans@utexas.edu>
2020-03-19 15:58:04 -0500

    First commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Using census data to understand the youth population size and (potentially) voting habits.',
+    author='Matt Stevans',
+    license='MIT',
+)
